e7388f37bcca2eefc829e3e720319016ac2adb49
Stalloyde <stalloyde93@gmail.com>
2023-06-07 12:12:47 +0800

    Enable adding of tech stack to form

--- src/Components/form.js
+++ src/Components/form.js
@@ -31,6 +31,64 @@ class DateInput extends React.Component {
   }
 }
 
+class TechStack extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { techStack: [], currentTech: '' };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ currentTech: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.setState({
+      techStack: this.state.techStack.concat(this.state.currentTech),
+      currentTech: '',
+    });
+  }
+
+  render() {
+    return (
+      <div className='form-tech-stack-container'>
+        <h1>Technology Stack</h1>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            <input
+              type='text'
+              value={this.state.currentTech}
+              onChange={this.handleChange}
+            ></input>
+          </label>
+
+          <input type='submit' value='Add'></input>
+
+          {this.state.techStack.length > 0 ? (
+            <ul>
+              {this.state.techStack.map((item) => (
+                <li>
+                  {item}
+                  <button className='remove-stack-btn'>Remove</button>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <ul>
+              <li>
+                List of Added technology
+                <button className='remove-stack-btn'>Remove</button>
+              </li>
+            </ul>
+          )}
+        </form>
+      </div>
+    );
+  }
+}
+
 class Form extends React.Component {
   render() {
     return (
@@ -53,22 +111,7 @@ class Form extends React.Component {
           <TextInput label='Portfolio Site:' />
           <TextInput label='LinkedIn:' />
         </div>
-        <div className='form-tech-stack-container'>
-          <h1>Technology Stack</h1>
-          <form>
-            <label>
-              <input type='text'></input>
-            </label>
 
-            <input type='submit' value='Add'></input>
-            <ul>
-              <li>
-                List of Added technology
-                <button className='remove-stack-btn'>Remove</button>
-              </li>
-            </ul>
-          </form>
-        </div>
         <div className='form-experience-container'>
           <h1>Professional Experience</h1>
           <TextInput label='Position:' />
@@ -78,11 +121,14 @@ class Form extends React.Component {
           <DateInput label='End Date:' />
           <button>Add</button>
         </div>
+
+        <TechStack />
+
         <div className='form-education-container'>
           <h1>Education</h1>
           <TextInput label='University:' />
           <TextInput label='City:' />
-          <div class='input-container'>
+          <div className='input-container'>
             <label>
               Level of Course:
               <select>
