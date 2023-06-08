e93ae921db405b6bd595e3979b741809c17f8b77
Stalloyde <stalloyde93@gmail.com>
2023-06-08 12:36:24 +0800

    Restructure forms to allow for multiple experience and education

--- src/Components/form.js
+++ src/Components/form.js
@@ -66,46 +66,43 @@ class TechStackForm extends React.Component {
 
   render() {
     return (
-      <div className='form-tech-stack-container'>
-        <h1>Technology Stack</h1>
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            <input
-              type='text'
-              value={this.state.currentTech.text}
-              onChange={this.handleChange}
-            ></input>
-          </label>
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          <input
+            type='text'
+            value={this.state.currentTech.text}
+            onChange={this.handleChange}
+            placeholder='Insert proficient technology here'
+          ></input>
+        </label>
 
-          <input type='submit' value='Add'></input>
+        <input type='submit' value='Add'></input>
 
-          {this.state.techStack && (
-            <ul>
-              {this.state.techStack.map((item) => (
-                <li key={item.id} id={item.id}>
-                  {item.text}
-                  <button
-                    id={item.id}
-                    className='remove-stack-btn'
-                    onClick={this.handleRemove}
-                  >
-                    Remove
-                  </button>
-                </li>
-              ))}
-            </ul>
-          )}
-        </form>
-      </div>
+        {this.state.techStack && (
+          <ul>
+            {this.state.techStack.map((item) => (
+              <li key={item.id} id={item.id}>
+                {item.text}
+                <button
+                  id={item.id}
+                  className='remove-stack-btn'
+                  onClick={this.handleRemove}
+                >
+                  Remove
+                </button>
+              </li>
+            ))}
+          </ul>
+        )}
+      </form>
     );
   }
 }
 
-class ExperienceForm extends React.Component {
+class ExperienceInputs extends React.Component {
   render() {
     return (
-      <div className='form-experience-container'>
-        <h1>Professional Experience</h1>
+      <div className='form-experience-inputs-container'>
         <TextInput label='Position:' />
         <TextInput label='Company:' />
         <TextInput label='City:' />
@@ -118,17 +115,18 @@ class ExperienceForm extends React.Component {
             maxLength={350}
           ></textarea>
         </label>
-        {this.props.children}
+        <div className='remove-btn'>
+          <button> Remove </button>
+        </div>
       </div>
     );
   }
 }
 
-class EducationForm extends React.Component {
+class EducationInputs extends React.Component {
   render() {
     return (
-      <div className='form-education-container'>
-        <h1>Education</h1>
+      <div className='form-education-inputs-container'>
         <TextInput label='University:' />
         <TextInput label='City:' />
         <div className='input-container'>
@@ -150,13 +148,47 @@ class EducationForm extends React.Component {
         <TextInput label='Degree:' />
         <DateInput label='Start Date:' />
         <DateInput label='End Date:' />
-        {this.props.children}
+        <div className='remove-btn'>
+          <button> Remove </button>
+        </div>
       </div>
     );
   }
 }
 
 class Form extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleAddExperience = this.handleAddExperience.bind(this);
+    // this.state = {
+    //   experience: {
+    //     position: '',
+    //     company: '',
+    //     city: '',
+    //     startDate: '',
+    //     endDate: '',
+    //     summary: '',
+    //   },
+    //   experienceList: [],
+    //   education: {
+    //     university: '',
+    //     city: '',
+    //     levelOfCourse: '',
+    //     degree: '',
+    //     startDate: '',
+    //     endDate: '',
+    //   },
+    //   educationList: [],
+    // };
+  }
+
+  handleAddExperience() {
+    this.setState({
+      experience: this.state.experience.concat(this.state.experience),
+    });
+    console.log(this.state.experience);
+  }
+
   render() {
     return (
       <div>
@@ -179,14 +211,24 @@ class Form extends React.Component {
           <TextInput label='LinkedIn:' />
         </div>
 
-        <ExperienceForm>
-          <button OnClick={this.handleAddExperience}>Add</button>
-        </ExperienceForm>
-        <TechStackForm />
+        <div className='form-tech-stack-container'>
+          <h1>Technology Stack</h1>
+          <TechStackForm />
+        </div>
 
-        <EducationForm>
-          <button OnClick={this.handleAddEducation}>Add</button>
-        </EducationForm>
+        <div className='form-experience-container'>
+          <h1>Professional Experiences</h1>
+          <ExperienceInputs />
+          <ExperienceInputs />
+          <button onClick={this.handleAddExperience}>Add Experience</button>
+        </div>
+
+        <div className='form-education-container'>
+          <h1>Education</h1>
+          <EducationInputs />
+          <EducationInputs />
+          <button onClick={this.handleAddEducation}>Add Education</button>
+        </div>
       </div>
     );
   }
